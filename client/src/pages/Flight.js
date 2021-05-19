ef22652a0f1694ff04c3a2f9b7c4b9493086b9a2
Andrew Makarevich <65646579+AndrewMakarevich@users.noreply.github.com>
2021-05-19 20:39:40 +0300

    fourth v

---
+++ client/src/pages/Flight.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Flight = ()=>{
+    return(
+        <div>
+          FLIGHT
+        </div>
+    )
+}
+
+export default Flight;
---
+++ client/src/pages/Flights.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Flights = ()=>{
+    return(
+        <div>
+          FLIGHTS
+        </div>
+    )
+}
+
+export default Flights;
