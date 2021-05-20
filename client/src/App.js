a1521318102ab5b1bf275a0089946d56134b57ca
Andrew Makarevich <65646579+AndrewMakarevich@users.noreply.github.com>
2021-05-20 12:01:12 +0300

    third v

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './App.css';
+import './AdaptiveMenuScript';
 import { BrowserRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 
 function App() {
   return (
     <BrowserRouter>
+        <div className="AppBar">
+        
+            <div className="mobileMenuVersion">
+              <span className="firstSpan"></span>
+              <span className="secondSpan"></span>
+              <span className="thirdSpan"></span>
+            </div>
+        
+            <div className="sectionsMenu">
+            <button color="primary"><a href="/admin">Admin</a></button>
+            <button color="primary"><a href="/login">Login</a></button>
+            <button color="primary"><a href="/registration">Registration</a></button>
+            <button color="primary"><a href="/flights">Flights</a></button>
+            </div> 
+          
+      </div>  
       <AppRouter />
     </BrowserRouter>
     
